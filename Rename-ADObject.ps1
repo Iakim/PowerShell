4dfc0ab5cffc627f2e7f4cc486fb77d115cd02f3
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2018-02-06 08:39:19 -0200

    Create Rename-ADObject.ps1

---
+++ Rename-ADObject.ps1
@@ -0,0 +1,22 @@
+###########################################################################
+# NOME: Script para adicionar ou remover usuários de grupos
+# AUTOR: Isaac de Moraes
+# COMENTÁRIO: Este script renomeia o displayname de um ou mais usuários.
+# HISTÓRICO DE VERSÃO: 1.0
+# 1.0 | 06/02/2018 - Versão inicial - Isaac de Moraes
+############################################################################
+
+$sessionAD = New-PSSession -ComputerName sserverad -Credential domain\user
+Import-Module activedirectory
+
+$old = Get-Content "C:\PowerShell\old.csv"
+$new = Get-Content "C:\PowerShell\new.csv"
+
+ForEach-Object {
+
+Rename-ADObject $old[0] -NewName $new[0]
+Rename-ADObject $old[1] -NewName $new[1]
+
+}
+
+Remove-PSSession $sessionAD
